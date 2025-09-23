09825d34c822e898f7c201b3bf31221685cccd48
regneguP <la237711@student.helha.be>
2025-09-23 18:09:22 +0200

    test8

--- Projet_ReseauPython/Main.py
+++ Projet_ReseauPython/Main.py
@@ -2,5 +2,5 @@ print ("Hello World !")
 print ("test version 0.0.3")
 print ("voici ce que j'ai ajouter dans le main")
 print ("voici ce que j'ai ajouter depuis la branche 2")
-
+print ("il ne reste que la branche nouvelle fonctionnalité")
  
