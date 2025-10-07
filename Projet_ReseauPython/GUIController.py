cb9ee3d9a1873dedaa1cd274fb7e13a145de43c8
leisrepi <la238369@student.helha.be>
2025-10-07 17:40:00 +0200

    Login button

--- Projet_ReseauPython/GUIController.py
+++ Projet_ReseauPython/GUIController.py
@@ -3,10 +3,17 @@ import GUIHandler
 
 class GUIController:
     def __init__(self):
-        self.menu = GUIHandler.MenuSimple(self)
+        self.menu = GUIHandler.LoginMenu(self)
 
     def on_button_click(self):
         print("Bouton cliqué depuis le contrôleur !")
+
+    def on_login(self, email, password):
+        print(f"Login avec Email: {email}, Mot de passe: {password}")
+        #TODO : verifier si les identifiants sont corrects avant d'ouvrir la fenetre principale
+        #TODO : ouvrir la fenetre en lui fournissant l'id de l'utilisateur
+        self.menu.root.destroy()
+        self.menu = GUIHandler.MenuSimple(self)
 
 if __name__ == "__main__":
     controller = GUIController()
--- Projet_ReseauPython/GUIHandler.py
+++ Projet_ReseauPython/GUIHandler.py
@@ -1,3 +1,26 @@
+class LoginMenu:
+	def __init__(self, controller):
+		self.controller = controller
+		self.root = tk.Tk()
+		self.root.title("Connexion")
+		self.root.geometry("400x250")
+
+		tk.Label(self.root, text="Email:", font=H3_FONT).pack(pady=(20, 0))
+		self.email_entry = tk.Entry(self.root, font=P_FONT, width=30)
+		self.email_entry.pack(pady=5)
+
+		tk.Label(self.root, text="Mot de passe:", font=H3_FONT).pack(pady=(10, 0))
+		self.password_entry = tk.Entry(self.root, show="*", font=P_FONT, width=30)
+		self.password_entry.pack(pady=5)
+
+		tk.Button(self.root, text="Se connecter", font=H3_FONT, command=self.on_login).pack(pady=20)
+
+		self.root.mainloop()
+
+	def on_login(self):
+		email = self.email_entry.get()
+		password = self.password_entry.get()
+		self.controller.on_login(email, password)
 
 class MenuSimple:
 	def __init__(self, controller):
@@ -11,6 +34,8 @@ class MenuSimple:
 
 	def on_button_click(self):
 		self.controller.on_button_click()
+
+        
 
 import tkinter as tk
 from tkinter import messagebox
