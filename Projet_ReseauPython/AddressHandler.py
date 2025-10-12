16e49c35d14ea591ebf1046b0ce8e4e6ea3ce1de
Tauh7“ <la235195@student.helha.be>
2025-10-12 15:40:08 +0200

    Formattage | To Do : adapter au changement

--- Projet_ReseauPython/AddressHandler.py
+++ Projet_ReseauPython/AddressHandler.py
@@ -1,18 +1,18 @@
 #To Do : renvoyer l'exception SNMaskErrorException
 from ipaddress import IPv4Address, IPv4Network
-import Subnet
+from Subnet import define_mask_by_ip_class#, create_network
 
 IpAddress = "192.168.3.55"
 SNMask = "255.255.128.0"
 
 #Retourne l'Ip du réseau et son adresse broadcast et le sous réseaux si possible
-def get_network_information_from_ip_address(IpAddress, SNMask):
+def get_network_information_from_ip_address_and_mask(IpAddress, SNMask):
     """Retourne l'adresse du réseaux et son broadcast et aussi le sous-réseaux avec son broadcast.
         Si le sous réseaux est impossible il renverra 'None' pour le sous réseaux et son broadcast.
         -------------------------------------------
         Peut lever l'exception SNMaskErrorException si le mask n'est pas correcte."""
     IpClient = IPv4Address(IpAddress)
-    netMask = Subnet.define_mask_by_ip_class(IpClient)
+    netMask = define_mask_by_ip_class(IpClient)
     IpNetwork = IPv4Network(IpAddress+"/"+netMask, strict=False)
     #vérification de l'appartenance du masque dde sous réseaux par rapport a celui du réseaux (vérification que celui-ci n'est pas plus grand)
     if(str(IpNetwork.netmask) < SNMask):
@@ -25,4 +25,4 @@ def get_network_information_from_ip_address(IpAddress, SNMask):
     SNBroadcast = IPv4Network(str(SNIpAddress)+"/"+SNMask, strict=False).broadcast_address
     return IpNetwork.network_address, IpNetwork.broadcast_address, SNIpAddress, SNBroadcast
 
-print(get_network_information_from_ip_address(IpAddress, SNMask))
+print(get_network_information_from_ip_address_and_mask(IpAddress, SNMask))
