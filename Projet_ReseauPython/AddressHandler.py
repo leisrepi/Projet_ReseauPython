bb51e5bf0a5995a977f78b3f4c42eda29acde256
Tauh7“ <la235195@student.helha.be>
2025-10-12 15:27:49 +0200

    Gestion exception a confirmer plus tard

--- Projet_ReseauPython/AddressHandler.py
+++ Projet_ReseauPython/AddressHandler.py
@@ -1,4 +1,4 @@
-import ipaddress
+#To Do : renvoyer l'exception SNMaskErrorException
 from ipaddress import IPv4Address, IPv4Network
 import Subnet
 
@@ -8,13 +8,15 @@ SNMask = "255.255.128.0"
 #Retourne l'Ip du réseau et son adresse broadcast et le sous réseaux si possible
 def get_network_information_from_ip_address(IpAddress, SNMask):
     """Retourne l'adresse du réseaux et son broadcast et aussi le sous-réseaux avec son broadcast.
-        Si le sous réseaux est impossible il renverra 'None' pour le sous réseaux et son broadcast"""
+        Si le sous réseaux est impossible il renverra 'None' pour le sous réseaux et son broadcast.
+        -------------------------------------------
+        Peut lever l'exception SNMaskErrorException si le mask n'est pas correcte."""
     IpClient = IPv4Address(IpAddress)
     netMask = Subnet.define_mask_by_ip_class(IpClient)
     IpNetwork = IPv4Network(IpAddress+"/"+netMask, strict=False)
     #vérification de l'appartenance du masque dde sous réseaux par rapport a celui du réseaux (vérification que celui-ci n'est pas plus grand)
     if(str(IpNetwork.netmask) < SNMask):
-        return None #gérer l'erreur dans le cas ou le sous réseaux est plus grand que le réseaux lui meme
+        raise #SNMaskErrorException #gérer l'erreur dans le cas ou le sous réseaux est plus grand que le réseaux lui meme
     if(IpNetwork.netmask == SNMask): #Vérification de la possibilité de sous-réseaux
         return IpNetwork.network_address, IpNetwork.broadcast_address, None, None
     for SNIp in IpNetwork.hosts(): #boucle de recherche du sous réseaux dans lequel se trouve l'adresse Ip du client
