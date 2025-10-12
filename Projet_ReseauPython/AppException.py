8dda4817b6e5999f9b9f79d8d210c71a4f9969a5
oracloP <la238366@student.helha.be>
2025-10-12 15:52:44 +0200

    ajout de l'exception InvalidMaskException

--- Projet_ReseauPython/AppException.py
+++ Projet_ReseauPython/AppException.py
@@ -2,3 +2,8 @@ class TooManyMachinesException(Exception):
     def __init__(self, __message):
         self.__message = __message
         super().__init__(self.__message)
+
+class InvalidMaskException(Exception):
+    def __init__(self, __message):
+        self.__message = __message
+        super().__init__(self.__message)
