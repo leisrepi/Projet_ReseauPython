ae2d2c530b412b8f93d2206051f9a78eaeeaee3f
oracloP <la238366@student.helha.be>
2025-10-13 17:43:26 +0200

    ajout de la méthode create_ip_address

--- Projet_ReseauPython/AddressHandler.py
+++ Projet_ReseauPython/AddressHandler.py
@@ -11,12 +11,34 @@ from AppException import TooManyMachinesException
 # --------------------------------------
 
 # Vérification de la validité de l'adresse IP. 
+# Vérification de la validité de l'adresse IP. 
 def is_ip_valid(address):  
-    """Vérifie si une adresse IP est valide"""
-    """Renvoie true si valide, false sinon"""
-    """address : chaîne de caractères de l'adresse IP"""
+    """Vérifie si une adresse IP est valide
+
+        Args:
+            address (IPv4Adress) : chaîne de caractères de l'adresse IP
+
+        Returns:
+            return (boolean) : Renvoie true si valide, false sinon
+    """
     try:
         ipadress = IPv4Address(address)
         return not ipadress.is_private
     except AddressValueError:
         return False
+    
+# Création d'une adresse IP à partir d'une chaîne de caractères (renvoie None si l'adresse n'est pas valide)
+def create_ip_adress(ip_string):
+    """Crée une adresse IP à partir d'une chaîne de caractères
+
+        Args:
+            ip_string (string): chaîne de caractères de l'adresse IP
+
+        Returns: 
+            return (IPv4Adress) : Renvoie une instance de IPv4Address ou lève une AdressValueError si l'adresse n'est pas valide
+    """
+    try:
+        return IPv4Address(ip_string)
+    except AddressValueError:
+        raise AddressValueError("Adresse IP non valide")
+    
