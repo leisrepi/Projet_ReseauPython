7b2f9d509b95faf68625483ccb409fdf104d3aaf
Tauh7“ <la235195@student.helha.be>
2025-10-14 14:24:04 +0200

    Ajout clé étrangères

--- Projet_ReseauPython/DBHandler.py
+++ Projet_ReseauPython/DBHandler.py
@@ -21,16 +21,18 @@ def createDB():
     AdresseIP varchar(20) not null,
     Masque varchar(20) not null,
     IsClassfull boolean not null default false,
-    primary key(idDR),
-    key(Pseudo)
+    primary key(idDR, Pseudo)
     )engine=innodb;
 
     CREATE TABLE IF NOT EXISTS SousReseau (
     NumSR smallint not null,
     NbMachine smallint not null default 0,
     IdDR smallint not null,
-    primary key(NumSR)
+    primary key(NumSR, IdDR)
     )engine=innodb;
+
+    CONSTRAINT fk_DecoupeReseau_Utilisateur FOREIGN KEY (Pseudo) REFERENCES Utilisateur(Pseudo) ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT fk_SousReseau_DecoupeReseau FOREIGN KEY (IdDR, Pseudo) REFERENCES DecoupeReseau(idDR, Pseudo) ON DELETE CASCADE ON UPDATE CASCADE
     ''')
 
     conn.commit() #Sauvegarde la création des données
