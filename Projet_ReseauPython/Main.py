bda1a2ba2eb3b336757b5b2c62d4b46d1fe57bd4
leisrepi <la238369@student.helha.be>
2025-10-16 19:11:02 +0200

    Ajout d'un sous process pour stocker la SECRET_KEY et effectuer les signature (+++ sécuriter)

--- Projet_ReseauPython/Main.py
+++ Projet_ReseauPython/Main.py
@@ -1,15 +1,20 @@
 import GUIHandlerOld
 import GUIController
+import AuthHandler
 
-print ("Hello World !")
-print ("test version 0.0.3")
-print ("voici ce que j'ai ajouter dans le main")
-print ("voici ce que j'ai ajouter depuis la branche 2")
-print ("il ne reste que la branche nouvelle fonctionnalité")
-print ("j'ai refussioner avec la branche main")
 
-#GUIHandlerOld.init_GUI()
+#Important ! permet au sous processus "_sign_process" de "AuthHandler" de se lancer correctement sous Windows
+if (__name__ == "__main__"):
+    print ("Hello World !")
+    print ("test version 0.0.3")
+    print ("voici ce que j'ai ajouter dans le main")
+    print ("voici ce que j'ai ajouter depuis la branche 2")
+    print ("il ne reste que la branche nouvelle fonctionnalité")
+    print ("j'ai refussioner avec la branche main")
 
-guiController = GUIController.GUIController.get_instance()
+    #GUIHandlerOld.init_GUI()
+
+
+    guiController = GUIController.GUIController.get_instance()
 
 
