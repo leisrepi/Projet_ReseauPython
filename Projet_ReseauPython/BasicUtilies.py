208daa34b4fee5857b49ee5de90d23198ef89967
leisrepi <la238369@student.helha.be>
2025-11-03 14:31:24 +0100

    Ajout du module BasicUtilies qui contient plein de petit fonction général et pratique

---
+++ Projet_ReseauPython/BasicUtilies.py
@@ -0,0 +1,10 @@
+def to_int(s: str):
+    try:
+        return int(s)
+    except ValueError:
+        return None
+def to_float(s: str):
+    try:
+        return float(s)
+    except ValueError:
+        return None
