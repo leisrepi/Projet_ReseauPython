3601b8707d86eb2a60c3bab3a7609055e85e4c30
leisrepi <la238369@student.helha.be>
2025-11-11 14:50:55 +0100

    Added round_up_to_power_of_two function

--- Projet_ReseauPython/BasicUtilies.py
+++ Projet_ReseauPython/BasicUtilies.py
@@ -1,3 +1,4 @@
+from math import ceil, log2
 def to_int(s: str):
     try:
         return int(s)
@@ -8,4 +9,13 @@ def to_float(s: str):
         return float(s)
     except ValueError:
         return None
-    
+def round_up_to_power_of_two(n):
+    """Arrondit un entier à la puissance de 2 supérieure
+
+        Args:
+            n (int) : entier à arrondir
+
+        Returns:
+            return (int) : Renvoie l'entier arrondi à la puissance de 2 supérieure
+    """
+    return 2 ** ceil(log2(n))
