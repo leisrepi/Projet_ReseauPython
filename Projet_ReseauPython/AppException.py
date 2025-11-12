a5c14fa92414d870b1347a97c56a157579bfa86f
Tauh7“ <la235195@student.helha.be>
2025-11-12 15:29:54 +0100

    Changement des messages d'erreurs

--- Projet_ReseauPython/AppException.py
+++ Projet_ReseauPython/AppException.py
@@ -14,16 +14,16 @@ class InvalidMaskException(Exception):
         super().__init__(self.__message)
 
 class UserAlreadyInDBException(Exception):
-    def __init__(self, __message):
-        self.__message = __message
+    def __init__(self):
+        self.__message = "L'utilisateur existe déja dans la base de données."
         super().__init__(self.__message)
 
 class SNMaskErrorException(Exception):
-    def __init__(self, __message):
-        self.__message = __message
+    def __init__(self):
+        self.__message = "Le masque de sous-réseaux est erroné."
         super().__init__(self.__message)
 
 class NotAuthentifyException(Exception):
-    def __init__(self, __message):
-        self.__message = __message
+    def __init__(self):
+        self.__message = "L'authentification a échoué, la session est échue."
         super().__init__(self.__message)
