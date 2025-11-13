8d1fdd41a97f7557932914a092a231207b23e464
Tauh7“ <la235195@student.helha.be>
2025-11-13 11:01:09 +0100

    Modif test

--- Projet_ReseauPython/TestDBFile.py
+++ Projet_ReseauPython/TestDBFile.py
@@ -2,11 +2,11 @@ import DBHandler as db
 import AuthHandler as ath
 if (__name__ == "__main__"):
 
-    
     db.create_db()
     db.insert_user("test","test")
     session = ath.create_session("test", "test")
     print(db.get_all_subnettings_of_user(session))
     db.insert_decoupe(session, "Bonjour", "192.168.0.0","255.255.255.0")
+    db.insert_decoupe(session, "Aurevoir", "192.168.0.0","255.255.255.0")
     print(db.get_all_subnettings_of_user(session))
     db.insert_sous_reseau(session, 0, 10, "Bonjour")
